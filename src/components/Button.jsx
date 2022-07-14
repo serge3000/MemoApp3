94e51b4c762cd72e3f86446c975fd0f79a0cda25
UDaiki <104532190+serge3000@users.noreply.github.com>
2022-07-15 04:17:26 +0900

    Create LogInScreen SignUpScreen

---
+++ src/components/Button.jsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Button() {
+  return (
+    <View style={styles.buttonContainer}>
+      <Text style={styles.buttonLabel}>Submit</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: '#467FD3',
+    borderRadius: 4,
+    alignSelf: 'flex-start',
+    marginBottom: 24,
+  },
+  buttonLabel: {
+    fontSize: 16,
+    lineHeight: 32,
+    paddingVertical: 8,
+    paddingHorizontal: 32,
+    color: '#ffffff',
+  },
+});
