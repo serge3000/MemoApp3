a24359f2a43c027222fc6fbce62fb55eac54e304
UDaiki <104532190+serge3000@users.noreply.github.com>
2022-07-28 12:04:02 +0900

    Improve ux

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -8,6 +8,7 @@ import firebase from 'firebase';
 
 import CircleButton from '../components/CircleButton';
 import KeyBoardSafeView from '../components/KeyboardSafeView';
+import { translateErrors } from '../utils';
 
 export default function MemoEditScreen(props) {
   const { navigation, route } = props;
@@ -27,7 +28,8 @@ export default function MemoEditScreen(props) {
           navigation.goBack();
         })
         .catch((error) => {
-          Alert.alert(error.code);
+          const errorMsg = translateErrors(error.code);
+          Alert.alert(error.title, errorMsg.description);
         });
     }
   }
